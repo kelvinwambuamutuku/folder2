35340002ff843de105a3915c6044d4341611b920
Wambua Kelvin <wambuakelvin650@gmail.com>
2023-02-17 19:24:03 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//DOM--document object model
+const list =Document.getElementById("languages")
+list.innerHML +="<li>rust</li>"
+const cpp=document.getElementById("cpp")
+cpp.textContent="jocker"
+const kel = document.querySelector("h3")
+kel.setAttribute("id","title")
+kel.removeAttribute("beginning")
